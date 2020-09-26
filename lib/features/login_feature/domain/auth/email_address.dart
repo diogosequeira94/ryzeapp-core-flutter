43202d2c26d0f87e8d19432d08318e1c3cf45d38
Diogo Sequeira <diogosequeira@hotmail.fr>
2020-09-26 17:47:38 +0100

    added freezed and dartz to pubspecyaml, created domain auth

--- lib/features/login_feature/domain/auth/email_address.dart
+++ lib/features/login_feature/domain/auth/email_address.dart
@@ -1,12 +1,37 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 
 class EmailAddress extends Equatable {
   final String emailValue;
-  const EmailAddress(this.emailValue) : assert(emailValue != null);
+
+  factory EmailAddress(String input){
+    assert(input != null);
+    return EmailAddress._(
+      validateEmailAddress(input)
+    );
+  }
+
+  const EmailAddress._(this.emailValue);
 
   @override
   List<Object> get props => [emailValue];
 
   @override
   String toString() => 'EmailAdress($emailValue)';
+}
+
+String validateEmailAddress(String input) {
+  const emailRegex =
+  r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+
+  if(RegExp(emailRegex).hasMatch(input)){
+    return input;
+  } else {
+    throw InvalidEmailException(failedEmailValue: input);
+  }
+}
+
+class InvalidEmailException implements Exception {
+  final String failedEmailValue;
+  InvalidEmailException({@required this.failedEmailValue});
 }
