b0f65e26c48260c9f58a4eb8d912678061de76c5
Diogo Sequeira <diogosequeira@hotmail.fr>
2021-06-17 18:17:38 +0100

    Added pages for QR Code scanning

--- lib/features/bottom_navigation_bar/presentation/notifications_page.dart
+++ lib/features/bottom_navigation_bar/presentation/notifications_page.dart
@@ -1,3 +1,5 @@
+import 'package:firebaseblocryze/repository/job_posts/models/job_post.dart';
+import 'package:firebaseblocryze/uikit/widgets/job_status_pill.dart';
 import 'package:flutter/material.dart';
 
 class NotificationsPage extends StatelessWidget {
@@ -22,11 +24,13 @@ class NotificationsPage extends StatelessWidget {
                       Icon(Icons.watch_later_outlined, color: Colors.black54)),
             ],
           ),
-          title: Text('Notifications', style: TextStyle(color: Theme.of(context).textTheme.headline6.color)),
+          title: Text('Notifications',
+              style: TextStyle(
+                  color: Theme.of(context).textTheme.headline6.color)),
         ),
         body: TabBarView(
           children: [
-            _buildNotificationDummy(),
+            _buildNotificationMock(context),
             _buildNotificationDummy(),
           ],
         ),
@@ -43,21 +47,53 @@ class NotificationsPage extends StatelessWidget {
         children: [
           Icon(Icons.notifications_none, size: 80, color: Colors.black45),
           const SizedBox(height: 24.0),
-          Text(
-              'You have no notifications right now.',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.w600
-              ),
+          Text('You have no notifications right now.',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
               textAlign: TextAlign.center),
           const SizedBox(height: 8.0),
-          Text('When you have job updates, messages or news, it will be showed here.',
+          Text(
+              'When you have job updates, messages or news, it will be showed here.',
               style: TextStyle(
                 fontSize: 16.0,
               ),
               textAlign: TextAlign.center),
         ],
       ),
+    );
+  }
+
+  _buildNotificationMock(BuildContext context) {
+    final jobPost = JobPost(
+        jobID: '213213',
+        title: 'Mock Job',
+        description: 'This is a Mock Job, not Real. Do not Work for free',
+        imageUrl: null,
+        status: 'Active',
+        city: 'Lisbon',
+        hourRate: '20€ / hour',
+        isRemote: true,
+        slotsAvailable: 1,
+        languages: ['Portuguese']);
+    return Column(
+      children: [
+        Card(
+          elevation: 3.0,
+          child: ListTile(
+            title: Text('Job Confirmation: Restaurant'),
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                JobStatusPill(jobStatus: 'Active'),
+              ],
+            ),
+            onTap: () {
+              Navigator.popAndPushNamed(context, '/job-confirmation-page',
+                  arguments: jobPost);
+            },
+          ),
+        ),
+        Divider(height: 1.0,),
+      ],
     );
   }
 }
