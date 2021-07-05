f23a56ce8b7ec3f510e6ba90d5015ef653c12ff6
DiogoSequeira <diogo.sequeira@ctw.bmwgroup.com>
2021-07-05 11:58:29 +0100

    adde more spacing to covid pandemic alert

--- lib/features/home_page/presentation/widgets/home_page/covid_pandemic_alert.dart
+++ lib/features/home_page/presentation/widgets/home_page/covid_pandemic_alert.dart
@@ -9,7 +9,7 @@ class CovidPandemicAlert extends StatelessWidget {
     return showAlert
         ? Padding(
             padding: const EdgeInsets.only(
-                left: 14.0, right: 14.0, top: 20.0, bottom: 6.0),
+                left: 14.0, right: 14.0, top: 16.0, bottom: 6.0),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(8.0),
               child: Container(
